e587161bf9f93506cc96ef4edc02a670d7bf456e
LimSeungHyun <koreas9408@gmail.com>
2022-03-08 14:42:44 +0900

    패키지 업로드 필요로 SampleXXX.java 파일 생성 - 추후 삭제 예정 mybatis 설정파일 추가 dsipatcher-servlet 추가

---
+++ src/main/java/com/project/pms/service/SampleService.java
@@ -0,0 +1,8 @@
+package com.project.pms.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+}
---
+++ src/main/java/com/project/pms/vo/SampleVO.java
@@ -0,0 +1,5 @@
+package com.project.pms.vo;
+
+public class SampleVO {
+
+}
