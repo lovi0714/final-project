2e921e40041b545ef3c858e160bc619852acd41a
dahoon <dahoon1006@gmail.com>
2022-03-25 22:02:15 +0900

    [관리자]
    1. 사용자 추가
    - Controller, Service, VO 로직 추가
    - Email 전송 기능 추가
    - 버튼 클릭 시 수신자 Email에 임시번호발급 로직 추가 예정(미완성)

--- src/main/java/com/project/pms/admin/service/AdminService.java
+++ src/main/java/com/project/pms/admin/service/AdminService.java
@@ -1,5 +1,17 @@
 package com.project.pms.admin.service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
 public class AdminService {
 
 }
---
+++ src/main/java/com/project/pms/emp/controller/EmailController.java
@@ -0,0 +1,39 @@
+package com.project.pms.emp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.pms.emp.service.EmailManagement;
+import com.project.pms.emp.vo.Email;
+
+@Controller
+@RequestMapping("/emp/*")
+public class EmailController {
+	
+	@Autowired
+	EmailManagement emailManagement; // 서비스를 호출하기 위한 의존성 주입
+	
+	@GetMapping("/signUp.do")
+	public String signUp() {
+		return "/emp/signUp";
+	}
+	
+	@PostMapping("/sendEmail.do") 
+	public String send(@ModelAttribute Email vo, Model m) {
+		try {
+			emailManagement.sendMail(vo); // vo(메일 관련 정보)를 sendMail 저장
+			m.addAttribute("msg", "이메일 발송 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+            m.addAttribute("msg", "이메일 발송 실패");
+		}
+		return "/emp/signUp"; // 이메일 전송 실패 시 다시 회원가입 페이지 이동.
+	} 
+	
+	
+}
